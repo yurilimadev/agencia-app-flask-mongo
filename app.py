37a1d2976046e99a7b68cf53269d25638e6c7ed7
Yuri Dimitri Ribeiro Lima <dimitri.limaf@gmail.com>
2024-12-25 19:04:52 -0300

    Adicionando página do admin com estrutura e estilo

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+from dotenv import dotenv_values
 from flask import Flask, render_template, url_for
-
+from flask_pymongo import PyMongo
 app = Flask(__name__)
+
+config = dotenv_values(".env")
+print(config)
+app.config['MONGO_URI'] = config['MONGO_URI']
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/area-admin')
+def login():
+    return render_template('area-admin.html')
